17dc6ccbaf67a9465317cb36039c401ecaf93ffb
Loknath <lokanath60@gmail.com>
2024-12-17 18:10:15 +0530

    Account created implemented

--- src/main/java/com/loki/accounts/entity/BaseEntity.java
+++ src/main/java/com/loki/accounts/entity/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.loki.accounts.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +9,6 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
